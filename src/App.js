d83b304ce6e09d44bffc14f260043725b05b41df
NadaYousry <nada.yousry95@gmail.com>
2022-06-20 11:29:53 +0200

    add scripts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import * as LazyLoading from "./LazyLoading.js";
-import { Suspense } from "react";
-import { Router, Switch } from "react-router-dom";
-import history from "./history";
+import Blog from "./components/Blog/Blog";
+import Blogs from "./containers/blogs";
+import Home from "./containers/home";
+
 function App() {
   return (
     <div className="App">
-      <Router history={history}>
-        <Suspense fallback={"loading..."}>
-          <Router history={history}>
-            <Switch>
-              {/* <Routes>  */}
-              <LazyLoading.Home path="/"  exact/>
-              <LazyLoading.Blogs path="/blogs"  exact/>
-              <LazyLoading.Blog path="/blogs/:id"  exact/>
-              {/* </Routes> */}
-            </Switch>
-          </Router>
-        </Suspense>
-      </Router>
+      <BrowserRouter>
+        <Routes> 
+          <Route path="/" element={<Home />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/blogs/:id" element={<Blog />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React, { Suspense } from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-// import reportWebVitals from "./reportWebVitals";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const rootElement = document.getElementById("root");
 ReactDOM.hydrate(
-  <Suspense fallback={"loading..."}>
+  <React.StrictMode>
     <App />
-  </Suspense>,
-  rootElement
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+reportWebVitals();
